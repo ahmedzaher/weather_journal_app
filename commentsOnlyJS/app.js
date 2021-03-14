736d0c9af77fb2cdecb4b9512dc63543f211af24
ahmedzaher <ahmed20120024>
2021-03-14 06:19:45 +0200

    Functions to post & get data

--- commentsOnlyJS/app.js
+++ commentsOnlyJS/app.js
@@ -1,4 +1,5 @@
 // Personal API Key for OpenWeatherMap API
+const OPEN_WEATHER_MAP_API_KEY = 'c25c31ded24738075dacfb7c5e7a1d0f';
 
 // Event listener to add function to existing HTML DOM element
 
@@ -6,7 +7,27 @@
 
 /* Function to GET Web API Data*/
 
+const getData = async (url = '') => {
+    const response = await fetch(url);
+    return response.json();
+}
+
 /* Function to POST data */
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    });
+    return response.json(); 
+  }
 
 
 /* Function to GET Project Data */
